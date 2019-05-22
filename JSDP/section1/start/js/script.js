6af0bfd7ba2fe1344ae45d57c985bf8174fa3c5a
NRaykov <nediotenev@gmail.com>
2019-05-22 15:12:07 +0300

    Module design pattern

--- JSDP/section1/start/js/script.js
+++ JSDP/section1/start/js/script.js
@@ -1,3 +1,57 @@
+//Module design pattern
+var chatModule = (function(){
+
+	var leadself = 'Me: ',
+			leadcomputer = "PC: ",
+			aSaid = ["This is a Cyber Chat"],
+			msgYes = "Yes, that's a great idea.",
+			msgNo =  "No, that must be a mistake.",
+			aSassyStuff = ["Like mold on books, grow myths on history.",
+		"She moved like a poem and smiled like a sphinx.",
+		"As long as we don’t die, this is gonna be one hell of a story.",
+		"She laughed, and the desert sang.",
+		"You’ve got about as much charm as a dead slug."];
+
+	//Echo method
+	function echo (msg) {
+		aSaid.push("<div>" + msg + "</div>");
+
+		//Displays only 6 lines of replies
+		var aSaidLenght = aSaid.length,
+				start = Math.max(aSaidLenght - 6, 0),
+				out = "";
+
+		for (var i = start; i < aSaidLenght; i++) {
+			out += aSaid[i];
+		}
+
+		document.querySelector('.advert').innerHTML = out;
+		document.getElementById('talk').innerHTML = '<span>' + msg + '</span>';
+	};
+
+
+	return {
+
+		talk: function (msg) {
+			echo(this.leadself + msg);
+		},
+		replayYesNo: function () {
+			// Ternary operator
+			var message = Math.random() > 0.5 ? msgYes : msgNo;
+
+			echo(this.leadcomputer + message);
+		},
+		saySassyStuff: function () {
+			var message = this.aSassyStuff[Math.floor(Math.random() * aSassyStuff.length)];
+
+			echo(this.leadcomputer + message);
+		},
+
+	}
+	
+})();
+
+
 //Object Literal design pattern
 var o = {
 	leadself: 'Me: ',
